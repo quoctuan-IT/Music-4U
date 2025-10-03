44b2445140151e20ae0a42e1d36718c7cfe04f00
quoctuan-IT <phamquoctuan041203@gmail.com>
2025-10-04 00:53:01 +0700

    Update Front-End UI

--- source/app/admin.py
+++ source/app/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from django import forms
-from django.db import models
 
 from .models import Song, Album, Genre, Artist
 
